c2792d706094f7109c003f9f4c732fce91c7e6f5
mxkum20 <70348358+mxkum20@users.noreply.github.com>
2020-09-02 21:55:00 +0530

    uploading the sample test
    uploading the sample test

---
+++ sampletest.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Created By ManojKumar for Test Demo
+p=tj
+q=rt
+sudo su - foss <<$p
+#clear
+ssh -q vldddv021.dev.intranet <<$q
+cd /opt/apache/corp/websites/mrit-dev1-30149-40150;
+#echo " log directory path on `hostname` is `pwd`"
+echo "===== list of files on server vldddv021.dev.intranet for log directory==="
+ls -ltrh;
+echo "======log directory path is ======"
+pwd;
+echo "====server name is ========="
+hostname;
+echo "====end of the file details"
+$q
+$p
+mail -a "output.log" -s "Jenkins notification for Test pipeline to run command on vlddjumpdv140 server and sudo to foss and then ssh to vldddv021 " mxkum20@centurylink.com </dev/null
